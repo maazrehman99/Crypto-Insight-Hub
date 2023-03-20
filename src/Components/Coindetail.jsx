a59709489e33355713d1d27630a83bd348e2ba1c
Maaz <maaz97965@gmail.com>
2023-03-20 20:27:03 +0500

    working on coin detail

--- src/Components/Coindetail.jsx
+++ src/Components/Coindetail.jsx
@@ -1,10 +1,11 @@
-import { Box } from '@chakra-ui/react'
+import { Box, HStack, Image, Radio, RadioGroup, Stat, StatLabel, Text, VStack } from '@chakra-ui/react'
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { Server } from '../index'
+import Coins from './Coins'
+import Errorcomponent from './Errorcomponent'
 import Loader from './Loader'
-
 
 const Coindetail = () => {
   const [Coin, setCoin] = useState({})
@@ -12,16 +13,14 @@ const Coindetail = () => {
   const [error, setError] = useState(false)
   const [page, setPage] = useState(1)
   const [currency, setCurrency] = useState("pkr")
-  const params= useParams()
+  const params = useParams()
   useEffect(() => {
     const fetchCoin = async () => {
       try {
         const { data } = await axios.get(`${Server}/coins/${params.id}`);
-
         setCoin(data)
         setLoading(false)
         console.log(data)
-
       } catch (error) {
         setLoading(false)
         setError(true)
@@ -29,15 +28,34 @@ const Coindetail = () => {
     }
     fetchCoin()
   }, [params.id])
+  if (error) return <Errorcomponent message={"Error While Recieving Data"} />
   console.log(params.id)
   return (
-  loading?<Loader/>:<>
-  <Box>
-    thsii
-  </Box>
-  
-  </>
+    loading ? <Loader /> : <>
+      <Box>
+        this
+      </Box>
+      {/* button */}
+      <RadioGroup value={currency} onChange={setCurrency} p={'8'} >
+        <HStack spacing={'4'} >
+          <Radio value="pkr" >PKR</Radio>
+          <Radio value="usd" >USD</Radio>
+          <Radio value="eur" >EUR</Radio>
+        </HStack>
+      </RadioGroup>
+      <VStack spacing={4} p={'16'} alignItems={'flex-start'} >
+        <Text alignSelf={"center"} fontSize={"small"} opacity={"0.7"} >
+          Last Updated on {Date(Coin.market_data.last_updated).split("G")[0]}
+        </Text>
+        <Image h={'16'} w={'16'} objectFit={"contain"} src={Coin.image.large}>
+        </Image>
+        <Stat>
+          <StatLabel>{Coin.name}</StatLabel>
+          
+        </Stat>
+      </VStack>
+
+    </>
   )
 }
-
 export default Coindetail
